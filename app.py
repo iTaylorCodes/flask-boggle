ed7fc04e13039c7c23f85f2be02ce6a93b4906bb
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-05-28 19:18:52 -0700

    Created templates

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
+from flask import Flask, render_template, redirect, session
 from boggle import Boggle
 
 boggle_game = Boggle()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecret'
+
+session['boggle_game'] = boggle_game
+
+@app.route('/')
+def show_game():
+    """Show game board"""
+
+    board = boggle_game.make_board()
+    session['board'] = board
+    highscore = session.get("highscore", 0)
+
+    return render_template('index.html', board=board, highscore=highscore)
--- test.py
+++ test.py
@@ -7,4 +7,10 @@ from boggle import Boggle
 class FlaskTests(TestCase):
 
     # TODO -- write tests for every view function / feature!
+    def test_home(self):
+        with app.test_client() as client:
+            res = client.get('/')
+            html = res.get_data(as_text=True)
 
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('<h1>Boggle</h1>', html)
