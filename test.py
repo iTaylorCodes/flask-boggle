0eddfc17af809b1095eb119d8aa6cd60c726619b
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-05-29 14:06:08 -0700

    Added Tests

--- test.py
+++ test.py
@@ -7,10 +7,52 @@ from boggle import Boggle
 class FlaskTests(TestCase):
 
     # TODO -- write tests for every view function / feature!
-    def test_home(self):
-        with app.test_client() as client:
-            res = client.get('/')
+
+    def setUp(self):
+        """Code to run before each test."""
+
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+    def test_home_screen(self):
+        """Tests that session info and HTML is displayed"""
+
+        with self.client:
+            res = self.client.get('/')
             html = res.get_data(as_text=True)
 
             self.assertEqual(res.status_code, 200)
             self.assertIn('<h1>Boggle</h1>', html)
+            self.assertIn('board', session)
+            self.assertIsNone(session.get('highscore'))
+            self.assertIn('Score:', html)
+            self.assertIn('Seconds Left:', html)
+
+    def test_valid_word(self):
+        """Test if submitted word is valid"""
+
+        with self.client as client:
+            with client.session_transaction() as sess:
+                sess['board'] = [["R", "N", "U", "R", "R"], 
+                                 ["R", "N", "U", "R", "R"], 
+                                 ["R", "N", "U", "R", "R"], 
+                                 ["R", "N", "U", "R", "R"], 
+                                 ["R", "N", "U", "R", "R"]]
+        res = self.client.get('/check-word?word=run')
+        self.assertEqual(res.json['result'], 'ok')
+
+    def test_invalid_word(self):
+        """Test if submitted word is invalid"""
+
+        self.client.get('/')
+        res = self.client.get('/check-word?word=happy')
+
+        self.assertEqual(res.json['result'], 'not-on-board')
+
+    def test_not_a_word(self):
+        """Test if submitted word is actually a word"""
+
+        self.client.get('/')
+        res = self.client.get('/check-word?word= grbuqiom')
+
+        self.assertEqual(res.json['result'], 'not-a-word')
